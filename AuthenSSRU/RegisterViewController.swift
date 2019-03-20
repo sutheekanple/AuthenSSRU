8b3a8f2af0cce0e41ba736f590c13f15a39502df
Student20 <student20@Student20.local>
2019-03-20 12:05:30 +0700

    Create ViewController Success

---
+++ AuthenSSRU/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  AuthenSSRU
+//
+//  Created by Student20 on 20/3/2562 BE.
+//  Copyright © 2562 Sutheekan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
