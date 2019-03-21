c2f0dd5a7dddb13113beb4478e0cfe7b70a8099b
Student20 <student20@Student20.local>
2019-03-21 10:42:53 +0700

    Get Password Success

--- AuthenSSRU/RegisterViewController.swift
+++ AuthenSSRU/RegisterViewController.swift
@@ -38,9 +38,20 @@ class RegisterViewController: UIViewController {
         print("You Click Upload")
         
         let name = nameTextField.text!
+        let user = userTextField.text!
+        let password = passwordTextField.text!
+        
+        
         
         
         print("name = \(name)")
+        print("user = \(user)")
+        print("password = \(password)")
+        
+        
+        
+        
+        
         
     }
     
